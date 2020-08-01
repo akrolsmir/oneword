a51ed8461dd46083bf83cecbb4189f8cd6ee6960
Austin Chen <akrolsmir@gmail.com>
2020-08-01 16:50:15 -0700

    Prettier timer

--- timer-vue.js
+++ timer-vue.js
@@ -3,14 +3,14 @@ Vue.component('timer', {
     return {
       startMs: 0,
       secondsElapsed: 0,
-    }
+    };
   },
   props: {
     length: [Number, String],
     onFinish: Function,
   },
   mounted() {
-    this.$parent.$on('reset-timer', () => this.startMs = 0);
+    this.$parent.$on('reset-timer', () => (this.startMs = 0));
     const updateProgress = async (timestamp) => {
       if (!this.length) {
         this.startMs = 0;
@@ -25,14 +25,13 @@ Vue.component('timer', {
         }
       }
       window.requestAnimationFrame(updateProgress);
-    }
+    };
     window.requestAnimationFrame(updateProgress);
   },
-  template:
-    `
+  template: `
 <progress class="progress" :value="secondsElapsed" :max="length"
   v-if="this.length > 0"
   :class="{'is-warning': length - 10 < secondsElapsed && secondsElapsed < length - 3,
     'is-danger': secondsElapsed > length - 3}"></progress>
-`
+`,
 });
