e8f1fd136a58a14e7604f4f6e87a182d1156c997
Shawn Xie <shawnx@Shawns-MacBook-Air.local>
2021-01-30 21:48:09 -0800

    clean up and also address race condition w/ concurrent updates

--- wordit.js
+++ wordit.js
@@ -64,16 +64,17 @@ const vueApp = new Vue({
       const room = await getRoom(this.room);
 
       if (room) {
-        // If the player's name collides with another user's,
-        // prepend adjectives until it is unique
-        /* TODO(shawnx): UNCOMMENT THIS BEFORE COMMIT */
+        /*
+         * TODO(shawnx): UNCOMMENT THIS BEFORE COMMIT
+         * If the player's name collides with another user's,
+         * prepend adjectives until it is unique
+         */
         // while (
         //   _.keys(room.players).includes(this.player.name) &&
         //   (this.user.guest || this.user.email != room.playerData[this.player.name].email)
         // ) {
         //   this.player.name = capitalize(randomWord('adjectives')) + ' ' + this.player.name;
         // }
-
         this.room = room;
         return await this.joinRoom();
       } else {
@@ -82,25 +83,6 @@ const vueApp = new Vue({
         return await this.resetRoom();
       }
     },
-    /**
-    Room: {
-        name: 'apple',
-        mod: 'shawn',
-        theme: 'none', // 'celebs', 'disney', 'horror', 'kitchen'
-        wordsPerPlayer: 7, // customizable
-        players: {
-          'alice': 0, // 0 points to start
-          'bob': 0,
-          'carol': 0
-        },
-        currentRound: {
-            cluer: 'alice',
-            state: 'CLUER_PICKING', // or 'NOT_STARTED'/'GUESSING'/'DONE'
-            allWords: [xxx, xxx, xxx]
-        },
-        history: [{round1}, {round2}... etc]
-    }
-    */
     async resetRoom() {
       this.room = {
         name: this.room.name,
@@ -108,7 +90,9 @@ const vueApp = new Vue({
         mod: '',
         // players changed from list to object so it can also store each player's points
         players: [this.player.name],
+        // Each round has its own object.
         currentRound: {
+          // states can be 'CLUER_PICKING' 'TOSS_IN_DECOYS' 'GUESSING' and 'DONE'
           state: 'CLUER_PICKING',
           // First player becomes the first one to pick a word and a clue
           clueGiver: this.player.name,
@@ -146,26 +130,16 @@ const vueApp = new Vue({
       await setRoom(this.room);
     },
     async cluerSelectsWord(w) {
-      this.room.currentRound.word = w;
+      await updateRoom(this.room, { 'currentRound.word': w });
       await this.saveWordToAllWordsInRoom(w);
     },
-    generatePlayerWordlist() {
-      const allWordsThisRound = this.room.currentRound.allWords;
-      console.log(allWordsThisRound);
-      // If the user refreshed their page, we preserve the word they had
-      if (allWordsThisRound && allWordsThisRound[this.player.name]) {
-        this.player.wordlist.push(allWordsThisRound[this.player.name]);
-      }
-      while (this.player.wordlist.length < this.numItemsPerPlayer) {
-        this.player.wordlist.push(randomWord('adjectives') + '-' + randomWord('nouns'));
-      }
-    },
     async saveWordToAllWordsInRoom(w) {
-      this.room.currentRound.allWords[this.player.name] = w;
+      const update = {};
+      update[`currentRound.allWords.${this.player.name}`] = w;
+      await updateRoom(this.room, update);
       if (_.keys(this.room.currentRound.allWords).length == this.room.players.length) {
-        this.room.currentRound.state = 'GUESSING';
+        await updateRoom(this.room, { 'currentRound.state': 'GUESSING' });
       }
-      await setRoom(this.room);
     },
     async submitClue() {
       if (_.keys(this.room.currentRound.allWords).length === 0) {
@@ -174,7 +148,6 @@ const vueApp = new Vue({
       if (this.room.currentRound.clue === '') {
         return alert('Write a clue for your phrase!');
       }
-      //-- move block below to nextStage()
       const indexToRemove = this.player.wordlist.indexOf(this.room.currentRound.word);
       if (indexToRemove > -1) {
         this.player.wordlist.splice(indexToRemove, 1);
@@ -182,7 +155,7 @@ const vueApp = new Vue({
       }
       this.room.currentRound.state = 'TOSS_IN_DECOYS';
       // room.currentRound.clue should already be updated due to bi-di binding
-      await setRoom(this.room);
+      await updateRoom(this.room, { 'currentRound.state': 'TOSS_IN_DECOYS' });
     },
     async submitDecoy(decoy) {
       this.saveWordToAllWordsInRoom(decoy);
@@ -195,13 +168,15 @@ const vueApp = new Vue({
         this.player.wordlist.splice(indexToRemove, 1);
         this.player.wordlist.push(randomWord('adjectives') + '-' + randomWord('nouns'));
       }
-      this.room.currentRound.votes[this.player.name] = vote;
+      const update = {};
+      update[`currentRound.votes.${this.player.name}`] = vote;
+      await updateRoom(this.room, update);
       // Total votes are players.length - 1 since clueGiver can't vote.
       if (_.keys(this.room.currentRound.votes).length >= this.room.players.length - 1) {
         this.room.currentRound.state = 'DONE';
         this.room.history.push(this.room.currentRound);
+        await setRoom(this.room);
       }
-      await setRoom(this.room);
     },
     async nextStage() {
       if (this.room.currentRound.state == 'CLUER_PICKING') {
@@ -230,9 +205,19 @@ const vueApp = new Vue({
         this.room.players.push(this.player.name);
         await this.saveRoom('playerData', 'players');
       }
-
       // generate playerWordlist so wordlist keeps some state after page refresh.
       this.generatePlayerWordlist();
+    },
+    generatePlayerWordlist() {
+      const allWordsThisRound = this.room.currentRound.allWords;
+      console.log(allWordsThisRound);
+      // If the user refreshed their page, we preserve the word they had
+      if (allWordsThisRound && allWordsThisRound[this.player.name]) {
+        this.player.wordlist.push(allWordsThisRound[this.player.name]);
+      }
+      while (this.player.wordlist.length < this.numItemsPerPlayer) {
+        this.player.wordlist.push(randomWord('adjectives') + '-' + randomWord('nouns'));
+      }
     },
     goHome() {
       unlistenRoom();
@@ -252,54 +237,6 @@ const vueApp = new Vue({
         [this.room.players[0], this.room.players[index]] = [this.room.players[index], this.room.players[0]];
         await this.saveRoom('players');
       }
-    },
-    wordForWord(category) {
-      return (
-        {
-          nouns: 'word',
-          verbs: 'verb',
-          adjectives: 'adjective',
-          compounds: 'compound',
-          custom: 'word',
-        }[category] || 'word'
-      );
-    },
-    hasSpecialCharacters(word) {
-      return /[\s~`!@#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?()\._]/g.test(word);
-    },
-    dedupe(clues, showCollisions = true) {
-      // const cluesToPlayers = {};
-      // for (const [player, clue] of Object.entries(clues)) {
-      //   let foundDupe = false;
-      //   for (const existingClue of Object.keys(cluesToPlayers)) {
-      //     // If an existing dupe is found, join the strings into the new clue
-      //     if (anyDupes(existingClue.split(' / '), clue)) {
-      //       foundDupe = true;
-      //       cluesToPlayers[existingClue].push(player);
-      //       cluesToPlayers[existingClue + ' / ' + clue] = cluesToPlayers[existingClue];
-      //       delete cluesToPlayers[existingClue];
-      //       break;
-      //     }
-      //   }
-      //   // Otherwise, create a new array for this clue.
-      //   if (!foundDupe) {
-      //     cluesToPlayers[clue] = [player];
-      //   }
-      // }
-      // const deduped = Object.entries(cluesToPlayers)
-      //   .filter(([clue, cluers]) => cluers.length == 1)
-      //   .map(([clue, cluers]) => `${cluers[0]} - ${clue}`);
-      // const conflicts = Object.entries(cluesToPlayers)
-      //   .filter(([clue, cluers]) => cluers.length > 1)
-      //   .map(([clue, cluers]) => cluers.join(' & ') + (showCollisions ? ` - ${clue}` : ''));
-      // let result = showCollisions ? '' : '(Uh, good luck.)';
-      // if (deduped.length > 0) {
-      //   result = `${deduped.join('\n')}`;
-      // }
-      // if (conflicts.length > 0) {
-      //   result += `\n\nCollisions:\n${conflicts.join('\n')}`;
-      // }
-      // return result;
     },
     async newRound(sameGuesser = false) {
       this.room.currentRound = {
@@ -352,9 +289,6 @@ const vueApp = new Vue({
         this.room.roundsInGame = 13;
       }
     },
-    keysEqual,
-    finished,
-    dropped,
     moment,
   },
   computed: {
@@ -372,26 +306,6 @@ const vueApp = new Vue({
         return this.player.name == this.room.players[0];
       }
     },
-    // Returns the set of open room names that matches the current `room.name`.
-    // filteredRoomNameSet() {
-    //   const filtered = new Set();
-    //   const roomNameRe = new RegExp(this.room.name, 'i');
-    //   for (const openRoom of this.allRooms) {
-    //     if (openRoom.name.match(roomNameRe)) {
-    //       filtered.add(openRoom.name);
-    //     }
-    //   }
-    //   return filtered;
-    // },
-    customWordList() {
-      // If there are any commas, parse as csv; else, parse with whitespace
-      let words = this.room.customWords.split(',');
-      if (words.length <= 1) {
-        words = this.room.customWords.split(/\s/);
-      }
-      // Lowercase and trim out whitespace; take out empty words
-      return words.map((w) => w.toLowerCase().trim()).filter((w) => w);
-    },
   },
 });
 
@@ -401,13 +315,6 @@ function capitalize(str) {
   return str ? str[0].toLocaleUpperCase() + str.substring(1) : '';
 }
 
-function unpush(array, value) {
-  const index = array.indexOf(value);
-  if (index !== -1) {
-    array.splice(index, 1);
-  }
-}
-
 // Extracts a node from an object tree by its path, like "redTeam.players"
 function getIn(object, path) {
   let node = object;
@@ -415,19 +322,6 @@ function getIn(object, path) {
     node = node[part];
   }
   return node;
-}
-
-// Keys are just arrays of ints
-function keysEqual(key1, key2) {
-  if (key1.length !== key2.length) {
-    return false;
-  }
-  for (let i = 0; i < key1.length; i++) {
-    if (key1[i] !== key2[i]) {
-      return false;
-    }
-  }
-  return true;
 }
 
 function nextClueGiver(lastGuesser, players) {
@@ -442,57 +336,4 @@ function randomWord(category = 'nouns', customWords = '') {
   const categories = { nouns, compounds, verbs, adjectives, custom };
   const words = categories[category];
   return words[Math.floor(Math.random() * words.length)];
-}
-
-// Key elements will be unique e.g. randomKey(3, 4) => [2, 1, 4]
-function randomKey(length, max) {
-  if (length > max) {
-    throw "Can't have a longer key than # of words shown!";
-  }
-
-  // Generate a list from [1...max], then shuffle, then slice.
-  const sequential = [...Array(max).keys()].map((i) => i + 1);
-  return shuffleArray(sequential).slice(0, length);
-}
-
-// keyType = 'encode', 'intercept', 'decode'
-function finished(keyType, round) {
-  return round[keyType].length === vueApp.KEY_LENGTH;
-}
-
-// votes = { alice: [1, 2, 3], ...}; players = ['alice', ...]
-// function finishedVoting(votes, players) {
-//   // Not done if any player has not yet voted
-//   if (!arrayContentsEqual(players, Object.keys(votes))) {
-//     return false;
-//   }
-//   // Done when every vote is not the default value of NO_VOTE
-//   return Object.values(votes)
-//     .flat()
-//     .every((v) => v !== NO_VOTE);
-// }
-
-function arrayContentsEqual(a, b) {
-  const bSet = new Set(b);
-  return a.length === b.length && a.every((v) => bSet.has(v));
-}
-
-// Returns how many of <team>'s messages have been intercepted
-function intercepted(team, history) {
-  return history
-    .flatMap((entry) =>
-      Object.values(entry[team].round.interceptVotes).map((vote) => keysEqual(vote, entry[team].round.key))
-    )
-    .reduce((a, b) => a + b, 0);
-}
-
-// Returns how many of <team>'s messages have not been correctly decoded
-function dropped(team, history) {
-  const sum = (a, b) => a + b;
-  // Abusing the fact that 0 + false + true = 1
-  return history
-    .flatMap((entry) =>
-      Object.values(entry[team].round.decodeVotes).map((vote) => !keysEqual(vote, entry[team].round.key))
-    )
-    .reduce(sum, 0);
 }
