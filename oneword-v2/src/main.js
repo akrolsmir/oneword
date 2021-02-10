6eae34d73e5c10892827e67e096f3aa9cecd2a50
Austin Chen <akrolsmir@gmail.com>
2021-02-10 11:31:34 -0600

    When navigating between pages, scroll to the top

--- oneword-v2/src/main.js
+++ oneword-v2/src/main.js
@@ -20,6 +20,13 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  },
 })
 
 export const app = createApp(App)
