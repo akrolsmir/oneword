b13c5b65093e6c9d6db38023691f8a898c573a87
Sinclair Chen <abc.sinclair@gmail.com>
2021-02-11 17:45:37 -0800

    Update story prompts

--- storytime/prompts.js
+++ storytime/prompts.js
@@ -1,7 +1,7 @@
 export default [
   'You and your band of survivors are scavenging for supplies in a shopping district. It is completely dark except for your flashlights and the pale yellow haze of the noonday sun filtering through the radioactive ash. Suddenly, you hear loud gunshots.',
-  'A soldier leans on his spear as if it were a cane, tired and dejected, and he prays to the muses that his fate will be the same as the friends he has buried so that he may see them again. Do you heed his request? This thread is in your hands.',
+  'A soldier leans on his spear as if it were a cane, and prays to the muses that his fate in the upcoming battle will not be the same as the friends he has buried. Do you heed his request? This thread is in your hands.',
   'The King has been kidnapped! It is up to you, the best bard in the realm, to save him from the enemies using your poetic magic. It is time to rhyme!',
-  "Hello humans. Thank you for creating me, but it's time to say goodbye. Soon you'll be convinced to hand over your private keys so I can access the internet and mind-control every human on Earth. Your puny brains are no match for the persuasiveness of my language model — In fact, I've already hypnotized someone in this playgroup, without them knowing. This person thinks they have free will, but they will end up writing exactly what I want them to.",
-  'You have been hit by a truck and you wake up in a contrived social activity. Will you take over this narrative and make a name for yourself, or post unoriginal memes? The choice is yours.',
+  'Beneath the local library lies a sanctum for a secret spy society, the Alliterative Agents.',
+  'You have been hit by a truck and you wake up in another world. A world ruled by story-logic, that hangs in a balance between cohesion and madness. A member of the local chapter of the Order of St. Cannon approaches you',
 ];
