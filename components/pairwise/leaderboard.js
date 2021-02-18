1a6a22c33a4f4ff3c5bfd7b659d6d8fd6c64ff56
Shawn Xie <shawnx@Shawns-MacBook-Air.local>
2021-02-18 09:46:15 -0800

    more concise scoring rules and made decoy submission more obvious

--- components/pairwise/leaderboard.js
+++ components/pairwise/leaderboard.js
@@ -91,8 +91,9 @@ Vue.component('leaderboard', {
     <br/>
     <div class="has-text-centered">
       <p>If nobody or everybody guesses the correct word pair, the clue giver scores 0 and all others score 2 </p> <br/>
-      <p>If some but not all players guess the correct word pair, the clue giver scores 3 and so does everyone who guessed correctly </p> <br/>
-      <p>Incorrect guesses each award 1 point to whoever threw the decoy pair that earned the guess </p>
+      <p>If some but not all players guess the correct word pair, the clue giver and the correct guessers each scores 3 </p> <br/>
+      <p>Incorrect guesses each award 1 point to whoever threw the clever decoy pair that earned the guess </p> <br/>
+      <p>Players take turns giving clues in order. The game ends when the first player reaches 30 points! </p>
     </div>
   </div>
 `,
