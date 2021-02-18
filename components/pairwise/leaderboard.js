7308946c2c110bfdf733d6ed8cdb898bfaf2ab0e
Austin Chen <akrolsmir@gmail.com>
2021-02-18 00:39:27 -0600

    Hackathon: Shawn's Wordit (#47)
    * working mvp
    
    * added next round button
    
    * fuzz tallypoints to minimize contention
    
    * cosmetic changes
    
    * changed scoring system
    
    * converted players from dict to array, centered buttons during guessing phase
    
    * removes old words from user's wordlist after they've used it for a real word or as a decoy
    
    * show player state, i.e. whether they are guessing or have submitted a clue, or are picking among decoys etc
    
    * added visibility to clue giver
    
    * more visibility into decoy tossers for the cluegiver
    
    * added leadershipboard and fixed edge cases for point tallying
    
    * refactored wordit-specific components to subdirectory
    
    * renamed leaderboard and added more descriptive context to player display while clue picker is picking
    
    * fixed bad leaderboard scoring algorithm
    
    * added instructions for first time players
    
    * added an alert for preventing submitting clue without phrase, or phrase without clue
    
    * various improvements
    
    * clean up and also address race condition w/ concurrent updates
    
    * displays all player guesses after round
    
    * fixed bug where clue was not submitting
    
    * changed decoys from preset word combos to flexible mix and match
    
    * modified css/html for decoy message and started migrating away from lodash
    
    * removes all dependenies on lodash
    
    * fixed bug in leaderboard
    
    * removed hacky alerts
    
    * renamed wordit to wordix
    
    * minor refactor
    
    * nested instructions under simple collapsible, and added submit behavior for decoys
    
    * added game end condition
    
    * renamed to pairwise, fixed scoring order, set winning score to 30
    
    Co-authored-by: Shawn Xie <shawnx@Shawns-MacBook-Air.local>

---
+++ components/pairwise/leaderboard.js
@@ -0,0 +1,117 @@
+Vue.component('leaderboard', {
+  data() {
+    return {};
+  },
+  props: {
+    history: Array,
+    state: String,
+    players: Array,
+    total: Number,
+  },
+  computed: {
+    tallyScores: function () {
+      // tallyScores only counts rounds that have been pushed to history
+      const leaderBoard = {};
+      //initiate leaderBoard at 0 for every current player
+      this.players.forEach((player) => {
+        leaderBoard[player] = 0;
+      });
+      // Each player's client computes point totals for everyone independently
+      this.history.forEach((round) => {
+        // If all players found the clueGiver's phrase
+        if (Object.values(round.votes).every((guess) => guess === round.word)) {
+          // all players from that round who are still in the room
+          Object.keys(leaderBoard).forEach((player) => {
+            // EXCEPT clueGiver gets 2pts automatically
+            if (player !== round.clueGiver && round.votes[player]) {
+              // player entry is guaranteed to exist, b/c forEach is against leaderboard
+              leaderBoard[player] += 2;
+            }
+          });
+        }
+        // If some players found the clueGiver's word combo but not all, clueGiver gets 3 points
+        else if (Object.values(round.votes).includes(round.word)) {
+          // But only if clueGiver is among the current players.
+          if (Number.isInteger(leaderBoard[round.clueGiver])) {
+            leaderBoard[round.clueGiver] += 3;
+          }
+          // And then also award 3 pts to every guesser still in the game who guessed correctly
+          Object.keys(leaderBoard).forEach((player) => {
+            // Note that clueGiver does not vote
+            if (round.votes[player] === round.word) {
+              leaderBoard[player] += 3;
+            }
+            // Incorrect guesses awards 1 point to whoever threw the decoy that earned the guess
+            else {
+              // Invert makes the mapping {word -> player} from allWords
+              const wordToPlayer = invert(round.allWords);
+              const playersVote = round.votes[player];
+              if (playersVote && wordToPlayer[playersVote]) {
+                // also give a point to whoever threw the decoy earned this player's guess
+                const goodDecoyTosser = wordToPlayer[playersVote];
+                if (leaderBoard[goodDecoyTosser]) {
+                  // check goodDecoyTosser exists, they might have left the game.
+                  leaderBoard[goodDecoyTosser] += 1;
+                }
+              }
+            }
+          });
+        }
+        // nobody guessed the word
+        else {
+          // ClueGiver gets 0 points but all other players of that round get 2pts automatically
+          Object.keys(leaderBoard).forEach((player) => {
+            // player only gets points if they voted that round.
+            if (round.votes[player]) {
+              leaderBoard[player] += 2;
+            }
+            // Invert makes the mapping {word -> player} from allWords
+            const wordToPlayer = invert(round.allWords);
+            const playersVote = round.votes[player];
+            if (playersVote && wordToPlayer[playersVote]) {
+              // also give a point to whoever threw the decoy earned this player's guess
+              const goodDecoyTosser = wordToPlayer[playersVote];
+              if (leaderBoard[goodDecoyTosser]) {
+                // check goodDecoyTosser exists, they might have left the game.
+                leaderBoard[goodDecoyTosser] += 1;
+              }
+            }
+          });
+        }
+      });
+      // sort the leaderBoard highest score first, then return
+      const sortedPlayerScores = Object.entries(leaderBoard).sort(
+        (playerScore1, playerScore2) => playerScore2[1] - playerScore1[1]
+      );
+      sortedPlayerScores.some((playerScore) => {
+        if (playerScore[1] >= this.total) {
+          this.$emit('gameover', playerScore);
+        }
+        return playerScore[1] >= this.total;
+      });
+      return sortedPlayerScores;
+    },
+  },
+  mounted() {},
+  template: `
+  <div v-if="state" class="box">
+    <div class="fancy has-text-centered">
+      <h1>Leaderboard</h1>
+    </div>
+    <br/>
+    <template v-for="playerScore in tallyScores">
+      <div class="has-text-centered" role="alert">
+        <strong>{{ playerScore[0] }}</strong>: {{ playerScore[1] }} points
+      </div>
+    </template>
+  </div>
+`,
+});
+
+function invert(obj) {
+  let inverted_obj = {};
+  Object.keys(obj).forEach((key) => {
+    inverted_obj[obj[key]] = key;
+  });
+  return inverted_obj;
+}
---
+++ components/pairwise/navbarshawn.js
@@ -0,0 +1,89 @@
+Vue.component('navbar', {
+  props: {
+    value: {
+      type: Object, // Should be a user object
+      // Only show login button on pages that use it.
+      default() {
+        return { hideLogin: false };
+      },
+    },
+  },
+  data: () => ({
+    burgerOpen: false,
+  }),
+  methods: {
+    logIn() {
+      firebase.analytics().logEvent('login');
+      window.open('login-widget.html', 'Sign In', 'width=400,height=600');
+    },
+    async logOut() {
+      firebase.analytics().logEvent('logout');
+      await firebase.auth().signOut();
+      // Reset user info
+      Object.assign(this.value, { id: '', email: '', supporter: '' });
+    },
+    referAmazon() {
+      firebase.analytics().logEvent('view_amazon', {
+        source: 'navbar',
+      });
+      window.open('https://amzn.to/3bbSpZn', '_blank');
+    },
+    referPremium() {
+      firebase.analytics().logEvent('view_promotion', {
+        source: 'navbar',
+      });
+      window.open('./supporter.html', '_blank');
+    },
+    referIncrypt() {
+      firebase.analytics().logEvent('view_promotion', {
+        source: 'navbar',
+      });
+      window.open('./incrypt.html', '_blank');
+    },
+  },
+  computed: {
+    isSupporter() {
+      return ['BASE', 'SPONSOR', 'ADMIN'].includes(this.value.supporter);
+    },
+  },
+  template: `
+<nav class="navbar has-shadow is-fixed-top"
+  style="font-weight: 500">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="./">
+      <span style="font-family: 'Merienda One', cursive; font-size: 24px; color:  #4a4a4a" >Wordix!</span>
+    </a>
+    <a role="button" class="navbar-burger burger" :class= "{'is-active': burgerOpen}" aria-label="menu"
+        data-target="navbuttons" @click="burgerOpen = !burgerOpen;">
+      <!-- burger/x icon: -->
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+  <div id="navbuttons" class="navbar-menu" :class = "{'is-active': burgerOpen}">
+    <div class="navbar-start">
+      <a class="navbar-item" @click="referIncrypt">
+        Game under development :P
+      </a>
+    </div>
+
+    <div class="navbar-end">
+      <a class="navbar-item" @click="referAmazon">Buy the board game</a>
+      <a v-if="value.id" class="navbar-item" @click="logOut">
+        Sign out, {{ value.name.split(' ')[0] }}?
+      </a>
+      <a v-else-if="!value.hideLogin" class="navbar-item" @click="logIn">
+        Sign in
+      </a>
+      <div v-if="!isSupporter" class="navbar-item">
+        <a class="button is-warning" @click="referPremium">
+          <strong>Become a supporter!</strong>
+        </a>
+      </div>
+      <a v-else class="navbar-item" @click="referPremium">Supporter page</a>
+    </div>
+  </div>
+</nav>
+  `,
+});
