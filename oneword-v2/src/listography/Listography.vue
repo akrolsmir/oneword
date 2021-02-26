309ec1a09992f37c827891636e627cec8ef6645b
Adrian Marple <adrian.marple@gmail.com>
2021-02-25 21:56:06 -0800

    Responding to Austin's code review.

--- oneword-v2/src/listography/Listography.vue
+++ oneword-v2/src/listography/Listography.vue
@@ -11,7 +11,7 @@
       <div style="width: 100%; text-align: center;">({{room.winningScore}} points to win)</div>
       <br>
       <div v-for="player in room.players">
-        {{player.name}}: {{player.score}}
+        {{player.name}}: {{playerScores[player.name]}}
       </div>
     </div>
     <div class="narrow card round">
@@ -19,20 +19,10 @@
            @mouseout="infoHover = false" 
            :class="{expanded: infoHover}"
            v-if="room.state !== 'START'">
-        <div class="type" v-if="card.type === '3FOLD'">
-          Threefold <img src="./info.png">
-          <div class="info">Write up to <strong>3</strong> answers.</div>
-          <div class="info">Try to match as <strong>MANY</strong> players as possible.</div>
-        </div>
-        <div class="type" v-if="card.type === '1ON1'">
-          One-on-One <img src="./info.png">
-          <div class="info"> Write up to <strong>10</strong> answers.</div>
-          <div class="info">Try to match as <strong>ONLY 1</strong> other player. </div>
-        </div>
-        <div class="type" v-if="card.type === 'FORGOTTEN4'">
-          Forgotten Four <img src="./info.png">
-          <div class="info">Write up to <strong>4</strong> answers.</div>
-          <div class="info">Try to match <strong>NO</strong> other players.</div>
+        <div class="type">
+          {{longName[card.type]}} <img src="./info.png">
+          <div class="info">Write up to <strong>{{listSize[card.type]}}</strong> answers.</div>
+          <div class="info">{{explanation[card.type]}}</div>
         </div>
       </div>
 
@@ -68,7 +58,7 @@
 
     <div class="list card" v-if="room.state === 'LISTING' || room.state === 'PREVIEW'">
       <strong style="font-size: 1.5em;">Your Responses</strong>
-      <div class="item" v-for="index in listSize" :key="index">
+      <div class="item" v-for="index in listSize[card.type]" :key="index">
         <div class="index">{{index}}</div>
         <textarea
           class="textarea mb-2"
@@ -82,18 +72,21 @@
     <div id="history">
       <div v-for="round in room.history" class="card summary">
         <div>
-          Round {{round.number}}: {{round.card.category}}
-          <span v-if="round.card.type === '3FOLD'">(Threefold)</span>
-          <span v-if="round.card.type === '1ON1'">(One-on-One)</span>
-          <span v-if="round.card.type === 'FORGOTTEN4'">(Forgotten Four)</span>
+          Round {{round.number + 1}}: {{round.card.category}} ({{longName[round.card.type]}})
         </div>
-        <div class="player" v-for="player in room.players">
-          {{player.name}}
-           scored <strong>{{round.scores[player.name]}}</strong>:&ensp;
-          <span v-for="index in round.entries[player.name].length">
-            <span :class="{fail: !entryScore(round.collisions[player.name][index-1], round)}">
-              {{round.entries[player.name][index-1]}}
-            </span> ({{round.collisions[player.name][index-1]}})&ensp;
+        <div class="player" v-for="(entries, name) in round.entries">
+          {{name}}
+           scored <strong>{{roundScores[round.number][name]}}</strong>:&ensp;
+          <span v-for="index in entries.length">
+            <span v-if="entries[index-1]">
+              <span :class="{invalid: room.invalidEntries[round.card.category + entries[index-1]]}">
+                <span :class="{fail: !entryScore(collisions[round.number][name][index-1], round)}">
+                  {{entries[index-1]}}
+                </span> ({{collisions[round.number][name][index-1]}})
+              </span>
+              <span class="trash" @click="toggleInvalid(round.card.category, entries[index-1])">🗑️</span>
+              &ensp;
+            </span>
           </span>
         </div>
       </div>
@@ -198,6 +191,13 @@
   .fail {
     text-decoration: line-through;
   }
+  .invalid {
+    opacity: 0.3;
+    text-decoration: line-through;
+  }
+  .trash {
+    cursor: pointer;
+  }
 
 </style>
 
@@ -230,7 +230,7 @@ export default {
           },
         ],
         round: {
-          number: 1,
+          number: 0,
           card: {
             type: "3FOLD",
             category: "Types of tree",
@@ -245,27 +245,111 @@ export default {
           },
         },
         history: [],
+        invalidEntries: {},
       },
       player: {
         name: "adrian",
       },
     }
   },
+  watch: {
+    playerScores() {
+      for (let player of this.room.players) {
+        if (this.playerScores[player.name] >= this.room.winningScore) {
+          this.endGame();
+          break;
+        }
+      }
+    },
+  },
   computed: {
     listSize() {
-      switch(this.card.type) {
-        case "3FOLD":
-          return 3;
-        case "1ON1":
-          return 10;
-        case "FORGOTTEN4":
-          return 4;
-        default:
-          return 0;
-      }
+      return {
+        '3FOLD': 3,
+        '1ON1': 10,
+        'FORGOTTEN4': 4,
+      };
+    },
+    longName() {
+      return {
+        '3FOLD': 'Threefold',
+        '1ON1': 'One on One',
+        'FORGOTTEN4': 'Forgotten Four',
+      };
+    },
+    explanation() {
+      return {
+        '3FOLD': "Try to match as MANY players as possible.",
+        '1ON1': "Try to match ONLY 1 other player.",
+        'FORGOTTEN4': "Try to match NO other players.",
+      };
     },
     card() {
       return this.room.round.card;
+    },
+    collisions() {
+      let collisions = [];
+
+      for (let round of this.room.history) {
+        collisions[round.number] = {};
+        for (let name in round.entries) {
+          collisions[round.number][name] = [];
+
+          for (let i = 0; i < round.entries[name].length; i++) {
+            let entry = round.entries[name][i];
+            if (!entry)
+              continue;
+            let key = round.card.category + entry;
+            if (this.room.invalidEntries[key])
+              continue;
+
+            collisions[round.number][name][i] = 0;
+            for (let otherName in round.entries) {
+              if (name === otherName)
+                continue;
+
+              if (round.entries[otherName].includes(entry)) {
+                collisions[round.number][name][i] += 1;
+              }
+            }
+          }
+        }
+      }
+      return collisions;
+    },
+    roundScores() {
+      let roundScores = [];
+      for (let round of this.room.history) {
+        roundScores[round.number] = {};
+        for (let name in round.entries) {
+          let score = 0;
+          for (let i = 0; i < round.entries[name].length; i++) {
+            let entry = round.entries[name][i];
+            if (!entry)
+              continue;
+            let key = round.card.category + entry;
+            if (this.room.invalidEntries[key])
+              continue;
+
+            score += this.entryScore(this.collisions[round.number][name][i], round);
+          }
+          roundScores[round.number][name] = score;
+        }
+      }
+      return roundScores;
+    },
+    playerScores() {
+      let scores = {};
+      for (let player of this.room.players) {
+        scores[player.name] = this.roundScores.reduce(
+          (total, scores) => total + scores[player.name],
+          0);
+      }
+      return scores;
+    },
+
+    previousCategories() {
+      return this.room.history.map(round => round.card.category);
     },
   },
   methods: {
@@ -275,48 +359,21 @@ export default {
     },
     nextStage() {
       this.room.state = "CHECKING";
-
-      this.room.round.scores = {};
-      this.room.round.collisions = {};
-
-      for (let player of this.room.players) {
-        let roundScore = 0;
-        this.room.round.collisions[player.name] = [];
-
-        for (let i = 0; i < this.room.round.entries[player.name].length; i++) {
-          let entry = this.room.round.entries[player.name][i];
-          var collisions = 0;
-          for (let other of this.room.players) {
-            if (player === other)
-              continue;
-
-            if (this.room.round.entries[other.name].includes(entry)) {
-              collisions += 1;
-            }
-          }
-          this.room.round.collisions[player.name][i] = collisions;
-          roundScore += this.entryScore(collisions, this.room.round);
-        }
-        this.room.round.scores[player.name] = roundScore;
-        player.score += roundScore;
-      }
-
       this.room.history.push(this.room.round);
-
-      for (let player of this.room.players) {
-        if (player.score >= this.room.winningScore) {
-          this.endGame();
-          break;
-        }
-      }
     },
     nextRound() {
       this.room.state = "PREVIEW";
 
       this.room.round = {};
-      this.room.round.card = cards[Math.floor(Math.random() * cards.length)];
+      for (let i = 0; i < 1000; i++) {
+        let card = cards[Math.floor(Math.random() * cards.length)];
+        if (!this.previousCategories.includes(card.category)) {
+          this.room.round.card = card;
+          break;
+        }
+      }
       this.room.round.entries = {};
-      this.room.round.number = this.room.history.length + 1;
+      this.room.round.number = this.room.history.length;
       for (let player of this.room.players) {
         this.room.round.entries[player.name] = [];
       }
@@ -336,6 +393,12 @@ export default {
       console.log("game over");
       // TODO
     },
+
+    toggleInvalid(category, entry) {
+      let key = category + entry;
+      this.room.invalidEntries[key] = !this.room.invalidEntries[key];
+    },
+
     entryScore(collisionCount, round) {
       switch(round.card.type) {
         case "3FOLD":
@@ -347,10 +410,11 @@ export default {
         default:
           return 0;
       }
-    }
+    },
   },
   async created() {
     // listenRoom("test-room", room => { this.room = room});
   },
 }
+
 </script>
