f635d083d4438b5f6e6758aec5775f1777c25d03
Shawn Xie <shawnx@Shawns-MacBook-Air.local>
2021-04-14 00:22:42 -0400

    removes duplicates from decoys and prevents submit w/o both pair and clue

--- src/pairwise/Pairwise.vue
+++ src/pairwise/Pairwise.vue
@@ -127,8 +127,9 @@
             </template>
             <template v-else>
               <template v-if="room.timers.running">
+                <span class="mx-4"><strong> Timers </strong></span>
                 <span v-if="room.timers.CLUER_PICKING" class="mx-2"
-                  >Word pair and clue: {{ room.timers.CLUER_PICKING }}s</span
+                  >Clueing: {{ room.timers.CLUER_PICKING }}s</span
                 >
                 <span v-if="room.timers.TOSS_IN_DECOYS" class="mx-2"
                   >Decoys: {{ room.timers.TOSS_IN_DECOYS }}s</span
@@ -164,11 +165,6 @@
             <span class="control">
               <button class="button is-small" @click="nextStage">
                 Next Stage
-              </button>
-            </span>
-            <span class="control">
-              <button class="button is-small" @click="newRound(true)">
-                Skip Word
               </button>
             </span>
             <span v-if="user.isAdmin" class="control">
@@ -217,7 +213,7 @@
             @kick="kickPlayer(playerScore[0])"
           ></Nametag>
         </div>
-        <div v-if="noMod">
+        <div v-if="noMod && room.people[player.name]?.state !== 'WATCHING'">
           <a @click="makeMod(player.name)"> (Become the mod...) </a>
         </div>
         <div v-else-if="player.isMod">
@@ -264,7 +260,13 @@
                 {{ word }}
               </button>
             </h2>
-            <label class="label">Your clue</label>
+            <br />
+            <label class="label"
+              >Your clue
+              <span v-if="player.showPickClueWarning" class="has-text-danger">
+                (pick a pair AND write a clue!)
+              </span>
+            </label>
             <div class="field has-addons">
               <div class="control is-expanded">
                 <input
@@ -274,13 +276,13 @@
                   v-model="player.currentClue"
                   @keyup.enter="submitClue"
                   :class="{ 'is-primary': true }"
+                  :disabled="!room.players.includes(player.name)"
                 />
               </div>
               <div class="control">
                 <!-- TODO: add tool tip to show why button is disabled https://wikiki.github.io/elements/tooltip/ -->
                 <button
                   class="button"
-                  onkeydown="return event.key != 'Enter';"
                   @click="submitClue"
                   :disabled="isClueSubmitDisabled()"
                 >
@@ -334,9 +336,13 @@
         <div v-if="room.currentRound.clueGiver != player.name">
           <h2 class="fancy has-text-centered" role="alert">
             Your clue from {{ room.currentRound.clueGiver }} is:
-            <strong>{{
-              room.wordsAndClues[room.currentRound.clueGiver].clue
-            }}</strong>
+            <strong
+              >"{{
+                room.wordsAndClues[room.currentRound.clueGiver]
+                  ? room.wordsAndClues[room.currentRound.clueGiver].clue
+                  : 'did not write a clue :/'
+              }}"</strong
+            >
           </h2>
           <br />
           <h2 class="has-text-centered" role="alert">
@@ -405,7 +411,9 @@
           <br />
           <div class="has-text-centered">
             <strong>{{
-              room.wordsAndClues[room.currentRound.clueGiver].clue
+              room.wordsAndClues[room.currentRound.clueGiver]
+                ? room.wordsAndClues[room.currentRound.clueGiver].clue
+                : 'did not write a clue :/'
             }}</strong>
           </div>
           <h2 class="fancy has-text-centered" role="alert">
@@ -472,7 +480,11 @@
             Your clue from {{ room.currentRound.clueGiver }}:
           </h2>
           <div class="fancy has-text-centered newline">
-            {{ room.wordsAndClues[room.currentRound.clueGiver].clue }}
+            {{
+              room.wordsAndClues[room.currentRound.clueGiver]
+                ? room.wordsAndClues[room.currentRound.clueGiver].clue
+                : 'did not write a clue :/'
+            }}
           </div>
         </div>
       </div>
@@ -484,7 +496,9 @@
         <h2 class="fancy has-text-centered" role="alert">
           The phrase from
           <strong>{{ room.currentRound.clueGiver }}</strong> was "{{
-            room.wordsAndClues[room.currentRound.clueGiver].word
+            room.wordsAndClues[room.currentRound.clueGiver]
+              ? room.wordsAndClues[room.currentRound.clueGiver].word
+              : 'did not pick a word :/'
           }}"!
         </h2>
         <br />
@@ -502,9 +516,9 @@
           points"!
         </div>
         <button class="button play-again" @click="newRound()">
-            Play Again
+          Play Again
         </button>
-        </div>
+      </div>
       <button v-else class="button" @click="newRound()">Next</button>
     </div>
     <br /><br />
@@ -604,6 +618,8 @@ function initializePlayerOnJoin(room, player) {
   player.currentWord = ''
   // cache's player's choice for clue on the player object, to reduce room update freq
   player.currentClue = ''
+  // whether to show pick pair AND write clue warning, used to guard clue submit
+  player.showPickClueWarning = false
   // how many entries in the pairList to pick out real pair
   player.choicesOfWordPairs = 7
   // pairlist array to choose from for picking out real pairs
@@ -718,15 +734,14 @@ export default {
   //   this.enterRoom()
   // },
   watch: {
-    // Timer currently not yet implemented for pairwise
     'room.currentRound.state'(state) {
-      // this.$emit('reset-timer')
       this.$refs.timer?.reset()
       if (state === 'CLUER_PICKING') {
         this.player.currentWord = ''
         this.player.currentClue = ''
       }
       if (state === 'TOSS_IN_DECOYS') {
+        // reset decoys wordlist
         this.player.decoyAdjList = this.generateDecoyWordList('adjectives')
         this.player.decoyNounList = this.generateDecoyWordList('nouns')
       }
@@ -816,6 +831,12 @@ export default {
       this.player.currentWord = w
     },
     async submitClue() {
+      // Show warning if attempting to submit w/ empty word and/or clue
+      if (!this.player.currentWord || !this.player.currentClue) {
+        this.player.showPickClueWarning = true
+        return
+      }
+      this.player.showPickClueWarning = false
       // sync chosen word and clue from local player to the room's list of words & clues
       this.room.wordsAndClues[`${this.player.name}`] = {
         word: this.player.currentWord,
@@ -895,7 +916,9 @@ export default {
       } else if (this.room.currentRound.state == 'TOSS_IN_DECOYS') {
         return await updateRoom(this.room, { 'currentRound.state': 'GUESSING' })
       } else if (this.room.currentRound.state == 'GUESSING') {
-        return await updateRoom(this.room, { 'currentRound.state': 'DONE' })
+        this.room.currentRound.state = 'DONE'
+        this.room.history.push(this.room.currentRound)
+        return await this.saveRoom('currentRound.state', 'history')
       } else {
         return await this.newRound()
       }
@@ -909,23 +932,27 @@ export default {
     },
     // Currently decoy wordlist is either all nouns or all adjectives
     generateDecoyWordList(type) {
-      const pairList = []
-      while (pairList.length < this.player.choicesPerDecoyCategory) {
-        pairList.push(randomWord(type))
-      }
-      return pairList
-    },
-    async kickPlayer(name) {
-      delete this.room.wordsAndClues[name]
-      if (this.room.players.includes(name)) {
-        const index = this.room.players.indexOf(name)
-        this.room.players.splice(index, 1)
-        if (this.room.currentRound.clueGiver === name) {
-          this.room.currentRound.clueGiver = this.room.players[0]
+      const list = []
+      while (list.length < this.player.choicesPerDecoyCategory) {
+        const w = randomWord(type)
+        if (!list.includes(w)) {
+          list.push(w)
         }
       }
-      await this.saveRoom('currentRound.clueGiver', 'players', 'wordsAndClues')
+      return list
     },
+    // TODO: REMOVE THIS
+    // async kickPlayer(name) {
+    //   delete this.room.wordsAndClues[name]
+    //   if (this.room.players.includes(name)) {
+    //     const index = this.room.players.indexOf(name)
+    //     this.room.players.splice(index, 1)
+    //     if (this.room.currentRound.clueGiver === name) {
+    //       this.room.currentRound.clueGiver = this.room.players[0]
+    //     }
+    //   }
+    //   await this.saveRoom('currentRound.clueGiver', 'players', 'wordsAndClues')
+    // },
     async newRound() {
       // delete current clueGiver's word and clue
       delete this.room.wordsAndClues[this.room.currentRound.clueGiver]
@@ -986,19 +1013,24 @@ export default {
       })
       // Each player's client computes point totals for everyone independently
       this.room.history.forEach((round) => {
-        const realWordThisRound = round.allWords[round.clueGiver]
+        const realPairThisRound = round.allWords[round.clueGiver]
         const historyThisRound = []
         // If all players found the clueGiver's phrase
         if (
+          // votes isn't an empty object
+          Object.keys(round.votes).length !== 0 &&
+          // clueGiver picked an actual pair
+          typeof realPairThisRound !== 'undefined' &&
+          // Every vote was for the real pair
           Object.values(round.votes).every(
-            (guess) => guess === realWordThisRound
+            (guess) => guess === realPairThisRound
           )
         ) {
           historyThisRound.push(
             `Wow, everyone guessed ` +
               round.clueGiver +
               `'s actual pair '` +
-              realWordThisRound +
+              realPairThisRound +
               `'!`
           )
           // all players from that round who are still in the room
@@ -1013,17 +1045,14 @@ export default {
           historyThisRound.push('And ' + round.clueGiver + ' gets no points :c')
         }
         // If some players found the clueGiver's word combo but not all
-        else if (Object.values(round.votes).includes(realWordThisRound)) {
+        else if (Object.values(round.votes).includes(realPairThisRound)) {
           historyThisRound.push(
-            round.clueGiver +
-              `'s actual phrase was '` +
-              realWordThisRound +
-              `'!`
+            round.clueGiver + `'s actual pair was '` + realPairThisRound + `'!`
           )
           // Award 3 pts to every guesser still in the game who guessed correctly
           Object.keys(leaderBoard).forEach((player) => {
             // Note that clueGiver does not vote
-            if (round.votes[player] === realWordThisRound) {
+            if (round.votes[player] === realPairThisRound) {
               leaderBoard[player] += 3
               historyThisRound.push(player + ' guessed right-- 3 points!')
             }
