ba9eb94028d5c2e1505f8f0dda6f1dc3f66737cf
Austin Chen <akrolsmir@gmail.com>
2021-04-29 22:35:30 -0400

    Revert "Try Heap analytics"
    This reverts commit 70f1aeb3ee37ad2193a449ea96607a9342dcd88a.

--- src/composables/useUser.js
+++ src/composables/useUser.js
@@ -54,10 +54,6 @@ export function useUser() {
   listenForLogin((newUser) => {
     Object.assign(user, newUser)
     user.guest = false
-    heap.identify(user.id)
-    heap.addUserProperties(
-      mask(user, 'name', 'supporter', 'createTime', 'lastUpdateTime')
-    )
   })
 
   return { user }
@@ -67,9 +63,4 @@ function buildAvatarSvg(color) {
   // SVG icon from https://remixicon.com/
   const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="-8 -8 40 40" fill="${color}" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M4 22a8 8 0 1 1 16 0H4zm8-9c-3.315 0-6-2.685-6-6s2.685-6 6-6 6 2.685 6 6-2.685 6-6 6z"/></svg>`
   return `data:image/svg+xml;utf8,${encodeURIComponent(svg)}`
-}
-
-// Clone the object, keeping only the specified props
-function mask(object, ...props) {
-  return Object.entries(props.map((prop) => [prop, object[prop]]))
 }
