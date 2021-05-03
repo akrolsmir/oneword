11129894def1f9cf98294211540793bc9e1039c7
Austin Chen <akrolsmir@gmail.com>
2021-05-03 16:49:22 -0400

    Retrofit Spectrum to Manticore rules

--- src/spectrum/Spectrum.vue
+++ src/spectrum/Spectrum.vue
@@ -9,9 +9,39 @@
     </template>
 
     <div class="fullscreen">
-      <h2>Mountain</h2>
-      <input type="range" orient="vertical" />
-      <h2>Molehill</h2>
+      <div v-if="room.state !== 'START'">
+        <h2 class="title">{{ room.round.card.from }}</h2>
+        <input
+          type="range"
+          orient="vertical"
+          v-model="room.round.guess1"
+          step="10"
+        />
+        <h2 class="title">{{ room.round.card.to }}</h2>
+      </div>
+      <h1 class="subtitle">{{ room.state }}</h1>
+      <button class="button" @click="nextStage">Next Stage</button>
+      <button class="button" @click="resetRoom">Reset Room</button>
+
+      <div v-if="room.state === 'PREVIEW'">
+        Currently in Preview
+        <!-- Show both areas but not the target -->
+      </div>
+
+      <div v-if="room.state === 'CLUE1'">
+        Now in Clue1
+        <!-- Show the target & an entry box for the cluer -->
+      </div>
+
+      <div v-if="room.state === 'GUESS2'">
+        Now in Guess2
+        <!-- Show both areas but not the target -->
+      </div>
+
+      <div v-if="room.state === 'END'">
+        Now it's over!
+        <!-- Show the result -->
+      </div>
     </div>
   </BigColumn>
 </template>
@@ -51,12 +81,12 @@ import {
   orderedEntries,
   pickFromBag,
 } from '../utils'
+import { nouns } from '../words/parts-of-speech'
 
 function makeNewRoom(name) {
   return {
     name,
-    state: 'START', // or "PREVIEW", "SEERING", "GUESSING", "END"
-    winningScore: 100,
+    state: 'START', // or "PREVIEW", "CLUE1", "GUESS1", "CLUE2", "GUESS2", "END"
     people: {},
     round: {
       /** Example:
@@ -64,16 +94,22 @@ function makeNewRoom(name) {
         from: 'Mountain'
         to: 'Molehill'
       },
-      target: 42, // from: 100, to: 0
-      seer: 'Austin',
-      guesses: {
-        'Alex': '30',
-        'Sinclair': '
-      }
+      target: 7, // from: 10 to 0, but only 9 to 1 are valid; 0 = shoot for moon
+      cluer: 'Austin',
+      clue1: 'My laundry pile'
+      guess1: 4,
+      clue2: 'Everest',
+      guess2: 9,
       */
     },
+
     history: [],
-    // timerLength: 90,
+    timers: {
+      clue1: 90,
+      guess1: 90,
+      clue2: 30,
+      guess2: 30,
+    },
     public: true,
     lastUpdateTime: Date.now(),
   }
@@ -99,33 +135,43 @@ export default {
   computed: {},
   methods: {
     sanitize,
-    toSeering() {
-      this.room.state = 'SEERING'
-      this.saveRoom('state')
-    },
-    toGuessing() {
-      this.room.state = 'GUESSING'
-      this.saveRoom('state')
-    },
-    toEnd() {
-      this.room.state = 'END'
-      this.room.history.push(this.room.round)
-      this.saveRoom('state', 'history')
-    },
-    nextRound() {
-      this.room.state = 'PREVIEW'
+    nextStage() {
+      const propsToSave = ['state']
 
-      this.room.round = {
-        // TODO: Actually update these each round
-        card: {
-          from: 'Mountain',
-          to: 'Molehill',
-        },
-        seer: this.players[0],
-        guesses: {},
+      switch (this.room.state) {
+        case 'PREVIEW':
+          this.room.state = 'CLUE1'
+          break
+        case 'CLUE1':
+          this.room.state = 'GUESS2' // Note: Skips GUESS1 and CLUE2 for now
+          break
+        case 'GUESS1':
+          // TODO: Should depend on the results. But assume always clue2 for now
+          this.room.state = 'CLUE2'
+          break
+        case 'CLUE2':
+          this.room.state = 'GUESS2'
+          break
+        case 'GUESS2':
+          this.room.state = 'END'
+          this.room.history.push(this.room.round)
+          propsToSave.push('history')
+          break
+        case 'START':
+        case 'END':
+          this.room.state = 'PREVIEW'
+          this.room.round = {
+            // TODO: Actually randomly generate these each round
+            card: {
+              from: pickRandom(nouns),
+              to: pickRandom(nouns),
+            },
+            target: Math.floor(Math.random() * 10), // Random from 0 to 9
+            guesser: this.room.players[0], // TODO: randomly re-pick
+          }
+          propsToSave.push('round')
       }
-
-      this.saveRoom('state', 'round')
+      this.saveRoom(...propsToSave)
     },
   },
 }
