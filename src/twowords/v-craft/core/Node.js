f6861474c3ef6408f2de08d2eabfd0da3ac5889a
Austin Chen <akrolsmir@gmail.com>
2021-06-15 12:48:59 -0700

    Prettier v-craft Node.js

--- src/twowords/v-craft/core/Node.js
+++ src/twowords/v-craft/core/Node.js
@@ -1,175 +1,191 @@
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid'
 
 class Node {
-  constructor(componentName, props = {}, parent = null, children = [], rules = {}, addition = {}) {
-    this.componentName = componentName;
-    this.props = props;
-    this.parent = parent;
-    this.children = children;
-    this.rules = rules;
-    this.addition = addition;
-    this.uuid = uuidv4();
+  constructor(
+    componentName,
+    props = {},
+    parent = null,
+    children = [],
+    rules = {},
+    addition = {}
+  ) {
+    this.componentName = componentName
+    this.props = props
+    this.parent = parent
+    this.children = children
+    this.rules = rules
+    this.addition = addition
+    this.uuid = uuidv4()
   }
 
   setProps(change) {
-    this.props = { ...this.props, ...change };
+    this.props = { ...this.props, ...change }
   }
 
   makeOrphan() {
-    const { parent } = this;
+    const { parent } = this
 
     if (!parent) {
-      return;
+      return
     }
 
-    const index = parent.children.indexOf(this);
-    parent.children.splice(index, 1);
-    this.parent = null;
+    const index = parent.children.indexOf(this)
+    parent.children.splice(index, 1)
+    this.parent = null
   }
 
   setParent(parent) {
     if (!parent.isDroppable(this)) {
-      throw new Error('Parent node is not droppable.');
+      throw new Error('Parent node is not droppable.')
     }
 
-    this.makeOrphan();
+    this.makeOrphan()
 
-    parent.children.push(this);
-    this.parent = parent;
+    parent.children.push(this)
+    this.parent = parent
   }
 
   inCanvas() {
-    let curentParent = this.parent;
+    let curentParent = this.parent
 
     while (curentParent) {
       if (curentParent.isCanvas()) {
-        return true;
+        return true
       }
-      curentParent = curentParent.parent;
+      curentParent = curentParent.parent
     }
 
-    return false;
+    return false
   }
 
   isDraggable() {
     if (!this.inCanvas()) {
-      return false;
+      return false
     }
 
     if (this.rules.canDrag) {
-      return this.rules.canDrag(this);
+      return this.rules.canDrag(this)
     }
 
-    return true;
+    return true
   }
 
   isAncestor(node) {
-    let curentParent = this.parent;
+    let curentParent = this.parent
 
     while (curentParent) {
       if (curentParent === node) {
-        return true;
+        return true
       }
-      curentParent = curentParent.parent;
+      curentParent = curentParent.parent
     }
 
-    return false;
+    return false
   }
 
   isDroppable(incommingNode) {
     if (!this.isCanvas()) {
-      return false;
+      return false
     }
 
     if (incommingNode === this) {
-      return false;
+      return false
     }
 
-    if (incommingNode.parent
-      && incommingNode.parent.rules.canMoveOut
-      && !incommingNode.parent.rules.canMoveOut(incommingNode, incommingNode.parent)) {
-      return false;
+    if (
+      incommingNode.parent &&
+      incommingNode.parent.rules.canMoveOut &&
+      !incommingNode.parent.rules.canMoveOut(
+        incommingNode,
+        incommingNode.parent
+      )
+    ) {
+      return false
     }
 
     if (this.isAncestor(incommingNode)) {
-      return false;
+      return false
     }
 
     if (this.rules.canMoveIn) {
-      return this.rules.canMoveIn(incommingNode, this);
+      return this.rules.canMoveIn(incommingNode, this)
     }
 
-    return true;
+    return true
   }
 
   isCanvas() {
     if (this.componentName === 'Canvas') {
-      return true;
+      return true
     }
 
-    return false;
+    return false
   }
 
   append(incommingNode) {
     if (!this.isDroppable(incommingNode)) {
-      throw new Error(`${this.componentName} is not droppable with the incommingNode - ${incommingNode.componentName}.`);
+      throw new Error(
+        `${this.componentName} is not droppable with the incommingNode - ${incommingNode.componentName}.`
+      )
     }
 
-    incommingNode.makeOrphan();
+    incommingNode.makeOrphan()
 
-    this.children.push(incommingNode);
+    this.children.push(incommingNode)
     // eslint-disable-next-line no-param-reassign
-    incommingNode.parent = this;
+    incommingNode.parent = this
   }
 
   prepend(incommingNode) {
     if (!this.isDroppable(incommingNode)) {
-      throw new Error(`${this.componentName} is not droppable with the incommingNode - ${incommingNode.componentName}.`);
+      throw new Error(
+        `${this.componentName} is not droppable with the incommingNode - ${incommingNode.componentName}.`
+      )
     }
 
-    incommingNode.makeOrphan();
+    incommingNode.makeOrphan()
 
-    this.children.splice(0, 0, incommingNode);
+    this.children.splice(0, 0, incommingNode)
     // eslint-disable-next-line no-param-reassign
-    incommingNode.parent = this;
+    incommingNode.parent = this
   }
 
   canBeSibling(targetNode) {
     if (targetNode === this) {
-      return false;
+      return false
     }
 
     if (!targetNode.parent) {
-      return false;
+      return false
     }
 
-    return targetNode.parent.isDroppable(this);
+    return targetNode.parent.isDroppable(this)
   }
 
   insertBefore(targetNode) {
     if (!this.canBeSibling(targetNode)) {
-      throw new Error('Can not be the sibling of the target node.');
+      throw new Error('Can not be the sibling of the target node.')
     }
 
-    this.makeOrphan();
+    this.makeOrphan()
 
-    const parentOfTargetNode = targetNode.parent;
-    const indexOfTargetNode = parentOfTargetNode.children.indexOf(targetNode);
-    parentOfTargetNode.children.splice(indexOfTargetNode, 0, this);
-    this.parent = parentOfTargetNode;
+    const parentOfTargetNode = targetNode.parent
+    const indexOfTargetNode = parentOfTargetNode.children.indexOf(targetNode)
+    parentOfTargetNode.children.splice(indexOfTargetNode, 0, this)
+    this.parent = parentOfTargetNode
   }
 
   insertAfter(targetNode) {
     if (!this.canBeSibling(targetNode)) {
-      throw new Error('Can not be the sibling of the target node.');
+      throw new Error('Can not be the sibling of the target node.')
     }
 
-    this.makeOrphan();
+    this.makeOrphan()
 
-    const parentOfTargetNode = targetNode.parent;
-    const indexOfTargetNode = parentOfTargetNode.children.indexOf(targetNode);
-    parentOfTargetNode.children.splice(indexOfTargetNode + 1, 0, this);
-    this.parent = parentOfTargetNode;
+    const parentOfTargetNode = targetNode.parent
+    const indexOfTargetNode = parentOfTargetNode.children.indexOf(targetNode)
+    parentOfTargetNode.children.splice(indexOfTargetNode + 1, 0, this)
+    this.parent = parentOfTargetNode
   }
 
   serialize() {
@@ -179,24 +195,26 @@ class Node {
       children: this.children.map((node) => node.serialize()),
       addition: this.addition,
       uuid: this.uuid,
-    };
+    }
   }
 }
 
 Node.unserialize = (editor, nodeData, parent = null) => {
-  const node = new Node();
-  Object.assign(node, nodeData);
+  const node = new Node()
+  Object.assign(node, nodeData)
 
-  const craftConfig = editor.getCraftConfig(node);
+  const craftConfig = editor.getCraftConfig(node)
 
   if (craftConfig.rules) {
-    node.rules = craftConfig.rules;
+    node.rules = craftConfig.rules
   }
 
-  node.parent = parent;
-  node.children = nodeData.children.map((data) => Node.unserialize(editor, data, node));
+  node.parent = parent
+  node.children = nodeData.children.map((data) =>
+    Node.unserialize(editor, data, node)
+  )
 
-  return node;
-};
+  return node
+}
 
-export default Node;
+export default Node
