e411144777955e2516502a2dfc18aedde05dc498
Austin Chen <akrolsmir@gmail.com>
2021-06-26 21:15:33 -0700

    Add `/multiplay/...` endpoint to display 3 windows at once (#94)
    * Multiplay: test 3 games at once
    
    * Override the logged-in user when "?player=Blah"
    
    * Hide the extra navbar on Multiplay

---
+++ src/admin/Multiplay.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="columns is-gapless" height="100vh">
+    <div class="column" v-for="player in ['Alpha', 'Bravo', 'Charlie']">
+      <iframe
+        :src="`/${game}/${id}?player=${player}`"
+        frameborder="0"
+        scrolling="no"
+      ></iframe>
+    </div>
+  </div>
+</template>
+
+<style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
+
+iframe {
+  border: 2px solid rgb(0, 0, 0);
+  width: 100%;
+  height: 3000px;
+}
+</style>
+
+<script setup>
+import { inject, onMounted } from '@vue/runtime-core'
+import { useRoute } from 'vue-router'
+
+// Hide the Navbar since each multiplay window already has it
+const showNavbar = inject('showNavbar')
+onMounted(() => {
+  showNavbar(false)
+})
+
+const route = useRoute()
+const game = route.params.game
+const id = route.params.id
+
+// TODOs:
+// - Automatically resize window height to iframe
+</script>
