fcc42446e5c595146d0a66fad295e7b1faf4a889
Austin Chen <akrolsmir@gmail.com>
2021-07-14 09:49:26 -0700

    Compile and run logic (highly buggy atm)

--- src/composables/useStore.js
+++ src/composables/useStore.js
@@ -45,6 +45,9 @@ export function useStore() {
   watch(
     () => cloneDeep($roomx),
     (roomx, prev) => {
+      // Run game logic and update room as appropriate
+      compute(roomx)
+
       // Identify the new paths in this room -- to scope down Firestore push
       const changes = flattenPaths(objectDiff(prev, roomx))
       if (!(isEmpty(changes) || 'name' in changes)) {
@@ -67,6 +70,25 @@ export function useStore() {
   }
 }
 
+function compute(room) {
+  try {
+    const sandbox = {
+      // Inject the room into the inputs function
+      inputs: (query) => inputs(room, query),
+      room,
+      Boolean,
+    }
+    // Just compile the rule that we need:
+    const compiled = compileCode(room.code[room.state])
+    // Run the code on our sandbox
+    compiled(sandbox)
+  } catch (e) {
+    // TODO: Map stack trace to user's code? And surface to user.
+    console.error(`Error with computing: ${e}`)
+    console.log('Code was:', room.code[room.state])
+  }
+}
+
 // Writes a value to a particular spot in the object, creating empty obj as necessary
 // Assumes object is defined
 export function setIn(object, path, value) {
@@ -81,4 +103,72 @@ export function setIn(object, path, value) {
   object[next] = object[next] || {}
   // Recurse!
   setIn(object[next], rest.join('.'), value)
+}
+
+// BUGS:
+// 1. Only one of the updates goes through (why? repro?)
+// 2. Arrays don't update correctly in Firestore
+// 3. Randomly infinite loops (repro?)
+// 4. Rerun logic on type = kinda annoying
+// 5. room.round = {} doesn't work, needs room.round = {b:1}
+
+// Eval code, but only accessing constants from the provided sandbox
+// From https://blog.risingstack.com/writing-a-javascript-framework-sandboxed-code-evaluation/
+// TODO: What happens if they infinite loop? What if they close the loop?
+// e.g. `} alert('hi')` totally breaks
+// Maybe solve with iframes and postMessage?
+// See: https://medium.com/zendesk-engineering/sandboxing-javascript-e4def55e855e
+const sandboxProxies = new WeakMap()
+
+function compileCode(src) {
+  src = `with (sandbox) {\n${src}\n}`
+  console.log(src)
+  const code = new Function('sandbox', src)
+
+  const has = (target, key) => true
+  const get = (target, key) =>
+    key === Symbol.unscopables ? undefined : target[key]
+
+  return function (sandbox) {
+    if (!sandboxProxies.has(sandbox)) {
+      const sandboxProxy = new Proxy(sandbox, { has, get })
+      sandboxProxies.set(sandbox, sandboxProxy)
+    }
+    return code(sandboxProxies.get(sandbox))
+  }
+}
+
+// Expand query strings by replacing `@ROLE` with the actual roles
+// E.g. 'CLUEING.@CLUER' => ['CLUEING.Austin', 'CLUEING.Alex'] => ['cat', 'dog']
+function inputs(room, query) {
+  const parts = query.split('.').map((part) => lookup(room, part))
+  const r = powerset(parts).map((array) =>
+    getIn(room, `round.${array.join('.')}`)
+  )
+  console.log('query', query, 'result', r)
+  return r
+}
+
+function lookup(room, part) {
+  if (part.startsWith('@')) {
+    // Return all players with this role
+    const role = part.slice(1)
+    return Object.entries(room.people)
+      .map(([name, entry]) => (entry.role === role ? name : ''))
+      .filter(Boolean)
+  }
+  // Not @ROLE syntax, so just return the original string wrapped in an array
+  return [part]
+}
+
+// Return a linear array of every possible combination
+// E.g. [[1], [2, 3], [4, 5]] => [[1, 2, 4], [1, 3, 4], [1, 2, 5], [1, 3, 5]]
+// TODO: encapsulate with unit tests: https://dev.to/vuesomedev/add-testing-to-vite-4b75
+function powerset(parts) {
+  if (parts.length == 0) {
+    return [[]]
+  }
+  const first = parts[0]
+  const rest = powerset(parts.slice(1))
+  return first.flatMap((f) => rest.map((r) => [f].concat(r)))
 }
