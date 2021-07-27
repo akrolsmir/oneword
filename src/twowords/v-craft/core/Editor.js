c19805b4d5ffd1483ad458e10b5cc9d2e2461234
Austin Chen <akrolsmir@gmail.com>
2021-07-26 17:10:43 -0700

    Prettier Editor.js

--- src/twowords/v-craft/core/Editor.js
+++ src/twowords/v-craft/core/Editor.js
@@ -1,101 +1,101 @@
-import kebabCase from 'lodash/kebabCase';
-import Indicator from './Indicator';
-import Node from './Node';
+import kebabCase from 'lodash/kebabCase'
+import Indicator from './Indicator'
+import Node from './Node'
 
 class Editor {
   constructor(nodes = [], resolverMap = {}) {
-    this.nodeMap = {};
-    this.selectedNode = null;
-    this.draggedNode = null;
-    this.indicator = new Indicator();
-    this.enabled = true;
+    this.nodeMap = {}
+    this.selectedNode = null
+    this.draggedNode = null
+    this.indicator = new Indicator()
+    this.enabled = true
 
-    this.setTopLevelNodes(nodes);
-    this.setResolverMap(resolverMap);
+    this.setTopLevelNodes(nodes)
+    this.setResolverMap(resolverMap)
   }
 
   enable() {
-    this.enabled = true;
+    this.enabled = true
   }
 
   disable() {
-    this.selectNode(null);
-    this.enabled = false;
+    this.selectNode(null)
+    this.enabled = false
   }
 
   setResolverMap(resolverMap) {
-    this.resolverMap = {};
+    this.resolverMap = {}
 
     Object.entries(resolverMap).forEach(([key, value]) => {
-      this.resolverMap[kebabCase(key)] = value;
-    });
+      this.resolverMap[kebabCase(key)] = value
+    })
   }
 
   initializeNodeMap(nodes) {
     nodes.forEach((node) => {
-      this.nodeMap[node.uuid] = node;
-      this.initializeNodeMap(node.children);
-    });
+      this.nodeMap[node.uuid] = node
+      this.initializeNodeMap(node.children)
+    })
   }
 
   setTopLevelNodes(nodes) {
-    this.nodes = nodes;
-    this.initializeNodeMap(nodes);
+    this.nodes = nodes
+    this.initializeNodeMap(nodes)
   }
 
   findNode(uuid) {
-    return this.nodeMap[uuid];
+    return this.nodeMap[uuid]
   }
 
   selectNode(node) {
-    this.selectedNode = node;
+    this.selectedNode = node
   }
 
   dragNode(node) {
-    this.draggedNode = node;
+    this.draggedNode = node
   }
 
   findResolver(name) {
-    return this.resolverMap[kebabCase(name)];
+    return this.resolverMap[kebabCase(name)]
   }
 
   removeNode(node) {
-    node.makeOrphan();
+    node.makeOrphan()
 
     if (node === this.selectedNode) {
-      this.selectNode(null);
+      this.selectNode(null)
     }
   }
 
   getCraftConfig(node) {
-    let resolver;
+    let resolver
     if (node.isCanvas()) {
-      resolver = this.findResolver(node.props.component);
+      resolver = this.findResolver(node.props.component)
     } else {
-      resolver = this.findResolver(node.componentName);
+      resolver = this.findResolver(node.componentName)
     }
 
-    return resolver.craft || {};
+    return resolver.craft || {}
   }
 
   getSettings(node) {
-    return this.getCraftConfig(node).settings || {};
+    return this.getCraftConfig(node).settings || {}
   }
 
   export() {
-    const nodesData = this.nodes.map((node) => node.serialize());
+    const nodesData = this.nodes.map((node) => node.serialize())
 
-    return JSON.stringify(nodesData);
+    return JSON.stringify(nodesData)
   }
 
   import(plainNodesData) {
     try {
-      const nodesData = JSON.parse(plainNodesData);
-      this.nodes = nodesData.map((data) => Node.unserialize(this, data));
+      const nodesData = JSON.parse(plainNodesData)
+      this.nodes = nodesData.map((data) => Node.unserialize(this, data))
     } catch {
-      throw new Error('The input is not valid.');
+      throw new Error('The input is not valid.')
     }
   }
 }
 
-export default Editor;
+export default Editor
