284d4cbd802b066b0afe3dcb72ba5f6de9c27fb3
Austin Chen <akrolsmir@gmail.com>
2021-08-26 18:11:51 -0700

    Make Publish tab write to ruleset metadata

--- src/studio/PublishTab.vue
+++ src/studio/PublishTab.vue
@@ -1,40 +1,90 @@
 <template>
-  <h1 class="title is-5 is-italic">(Not yet implemented)</h1>
+  <div class="columns">
+    <div class="column is-one-third">
+      <label class="label"
+        >Preview URL:
+        <a :href="`/preview/${room.name}`">boardless.games/{{ room.name }}</a>
+      </label>
+      <img
+        :src="
+          metadata.thumbnail || 'https://www.tibs.org.tw/images/default.jpg'
+        "
+      />
+    </div>
+    <div class="column">
+      <label class="label">Game title</label>
+      <input
+        type="text"
+        class="input"
+        v-model="metadata.title"
+        placeholder="One Word"
+      />
 
-  <label class="label"
-    >Preview URL:
-    <a :href="`/preview/${room.name}`">boardless.games/{{ room.name }}</a>
-  </label>
+      <label class="label">Thumbnail URL</label>
+      <input
+        type="text"
+        class="input"
+        v-model="metadata.thumbnail"
+        placeholder="https://..."
+      />
 
-  <label class="label">Game title</label>
-  <input type="text" class="input" />
-
-  <label class="label">Game description</label>
-  <textarea name="test" class="textarea"></textarea>
+      <label class="label">Game description</label>
+      <textarea
+        name="test"
+        class="textarea"
+        v-model="metadata.description"
+        placeholder="This is an online game based on Just One, a co-op word game for 3+ players.
+In each of the 13 rounds, come up with a one-word hint for the guesser...
+But beware: duplicate hints are discarded!"
+      ></textarea>
+    </div>
+  </div>
 
   <label class="label">Tags</label>
-  <input type="text" class="input" />
+  <input
+    type="text"
+    class="input"
+    v-model="metadata.tagsString"
+    placeholder="word, party, coop"
+  />
 
   <div class="columns">
     <div class="column">
       <label class="label">Min players</label>
-      <input type="number" class="input" />
+      <input
+        type="number"
+        class="input"
+        v-model="metadata.minPlayers"
+        placeholder="3"
+      />
     </div>
     <div class="column">
       <label class="label">Max players</label>
-      <input type="number" class="input" />
+      <input
+        type="number"
+        class="input"
+        v-model="metadata.maxPlayers"
+        placeholder="20"
+      />
     </div>
     <div class="column">
       <label class="label">Playtime in minutes</label>
-      <input type="number" class="input" />
+      <input
+        type="number"
+        class="input"
+        v-model="metadata.playtime"
+        placeholder="20"
+      />
     </div>
   </div>
 
-  <button class="button is-success">Publish</button>
+  <button class="button is-success" @click="publish">Publish</button>
+
+  <h1 class="title mt-6 mb-0">Modify game rules</h1>
 
   <div class="columns">
     <div class="column" v-for="listName in ['states', 'roles', 'testers']">
-      <label class="label">{{ listName }}</label>
+      <label class="label">{{ listName.toLocaleUpperCase() }}</label>
       <div class="content">
         <ol>
           <li v-for="item in room.rules[listName]">
@@ -70,15 +120,19 @@
 </style>
 
 <script>
+import cloneDeep from 'lodash/cloneDeep'
+
 export default {
   props: ['room'],
   data() {
     return {
-      local: {
-        state: '',
-        role: '',
-      },
+      local: {},
+      // Note: metadata is local, too =P
+      metadata: {},
     }
+  },
+  created() {
+    this.metadata = cloneDeep(this.room.metadata) || {}
   },
   methods: {
     // TODO: New states shouldn't null-ify
@@ -89,6 +143,10 @@ export default {
     remove(listName, item) {
       const filtered = this.room.rules[listName].filter((i) => i != item)
       this.room.rules[listName] = filtered
+    },
+    publish() {
+      this.room.metadata = cloneDeep(this.metadata)
+      alert('Successfully published!')
     },
   },
 }
