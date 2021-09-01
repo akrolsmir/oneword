8000c6726607415aa20220a4e03f01bb9e6cc013
Austin Chen <akrolsmir@gmail.com>
2021-09-01 13:17:16 -0700

    When v2 is undefined, call it out in the diff

--- src/utils.js
+++ src/utils.js
@@ -114,7 +114,7 @@ export function objectDiff(o1, o2) {
   for (const key of Object.keys(o2)) {
     const v1 = o1?.[key] // Allow o1 to be undefined
     const v2 = o2[key]
-    if (!isEqual(v1, v2)) {
+    if (!isEqual(v1, v2) || v2 === undefined) {
       if (typeof v2 === 'object' && !Array.isArray(v2)) {
         result[key] = objectDiff(v1, v2)
       } else {
