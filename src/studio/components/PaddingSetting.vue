a08d243e285795e22b9466d1208adc280df45d5f
Austin Chen <akrolsmir@gmail.com>
2021-09-02 01:14:15 -0700

    Add a setting for box padding

---
+++ src/studio/components/PaddingSetting.vue
@@ -0,0 +1,32 @@
+<template>
+  <BulmaDropdown
+    class="m-2"
+    :options="['0px', '8px', '16px', '24px', '32px']"
+    v-model="padding"
+  />
+</template>
+
+<script>
+import settingMixin from '../v-craft/components/settingMixin'
+import BulmaDropdown from '../../components/BulmaDropdown.vue'
+
+export default {
+  mixins: [settingMixin],
+  components: { BulmaDropdown },
+  computed: {
+    padding: {
+      get() {
+        return this.elementProps.padding
+      },
+      set(value) {
+        this.updateContent(value)
+      },
+    },
+  },
+  methods: {
+    updateContent(padding) {
+      this.elementPropsSetter({ padding })
+    },
+  },
+}
+</script>
